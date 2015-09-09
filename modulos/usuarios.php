70c119af484233b62ea494f37c06e1e18afaa658
Jefferson Lima <jefferson.carrenho@gmail.com>
2015-09-09 18:27:03 -0300

    Aula 10

--- modulos/usuarios.php
+++ modulos/usuarios.php
@@ -159,6 +159,65 @@ case 'incluir':
 break;
 case 'listar':
     echo '<h2>Usuários cadastrados</h2>';
+    loadCSS('data-tables', null, TRUE);
+    loadJS('jquery-datatable');
+    
+    ?>
+    
+    <script>
+        $(document).ready(function(){
+            $("#listausers").dataTable({
+                
+                "language": {
+                    "zeroRecords": "Nenhum dado encontrado",
+                    "info": "Mostrando página _PAGE_ de _PAGES_",
+                    "infoEmpty": "Sem registros disponíveis",
+                    "infoFiltered": "(filtrado de _MAX_ registros no total)",
+                    "search":"Pesquisar"
+                },
+                
+                "sSrollY": "400px",
+                "bPaginate": false,
+                "aaSorting" : [[0, "asc"]]
+            });
+        });
+    </script>
+    
+    <table class="display" id="listausers" cellpadding="0" border="0">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Login</th>
+                <th>Ativo/Adm</th>
+                <th>Cadastro</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            
+                $user = new usuarios();
+                $user->selecionaTudo($user);
+                
+                while($res=$user->retornaDados()):
+                    echo '<tr>';
+                        printf('<td>%s</td>', $res->nome);
+                        printf('<td>%s</td>', $res->email);
+                        printf('<td>%s</td>', $res->login);
+                        printf('<td class="center">%s/%s</td>', strtoupper($res->ativo),strtoupper($res->administrador));
+                        printf('<td class="center">%s</td>', date("d/m/Y", strtotime($res->datacad)));
+                        printf('<td class="center"><a href="?m=usuarios&t=incluir" title="Novo Cadastro"><img src="images/add.png" alt="Novo Cadastro" /></a> <a href="?m=usuarios&t=editar?id=%s"><img src="images/edit.png" alt="Editar" /></a> <a href="?m=usuarios&t=senha?id=%s"><img src="images/pass.png" alt="Mudar Senha" /></a> <a href="?m=usuarios&t=excluir?id=%s"><img src="images/delete.png" alt="Excluir" /></a> </td>',$res->id,$res->id,$res->id);
+                    echo '</tr>';
+                endwhile;
+            ?>
+        </tbody>
+    </table>
+    
+    
+    
+    <?php
+
 break;
 
 default:
